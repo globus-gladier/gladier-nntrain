8abb89b0cf6418d3c20955a056809a70867662ad
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-05-24 17:05:34 -0500

    bragnn

---
+++ BraggNN/tools/model_train.py
@@ -0,0 +1,18 @@
+from gladier import GladierBaseTool, generate_flow_definition
+
+def model_train(wdir, cmde):
+    cmd_aug = cmde.split('#')
+    import subprocess, os
+    os.chdir(wdir) 
+    result = subprocess.run(cmd_aug, stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8')
+
+
+@generate_flow_definition
+class ModelTrain(GladierBaseTool):
+    funcx_functions = [model_train]
+    required_input = [
+        'wdir',
+        'cmde', 
+        'funcx_endpoint_compute'
+        ]
---
+++ BraggNN/tools/transfer_model.py
@@ -0,0 +1,41 @@
+from gladier import GladierBaseTool
+
+
+class TransferModel(GladierBaseTool):
+
+    flow_definition = {
+        'Comment': 'Transfer a file or directory in Globus',
+        'StartAt': 'TransferModel',
+        'States': {
+            'TransferModel': {
+                'Comment': 'Transfer a file or directory in Globus',
+                'Type': 'Action',
+                'ActionUrl': 'https://actions.automate.globus.org/transfer/transfer',
+                'Parameters': {
+                    'source_endpoint_id.$': '$.input.comp_endpoint',
+                    'destination_endpoint_id.$': '$.input.dest_endpoint',
+                    'transfer_items': [
+                        {
+                            'source_path.$': '$.input.mdl_path',
+                            'destination_path.$': '$.input.dest_path',
+                            'recursive.$': '$.input.transfer_recursive',
+                        }
+                    ]
+                },
+                'ResultPath': '$.TransferModel',
+                'WaitTime': 600,
+                'End': True
+            },
+        }
+    }
+
+    flow_input = {
+        'transfer_sync_level': 'checksum',
+        'transfer_recursive': True,
+    }
+    required_input = [
+        'comp_endpoint',
+        'mdl_path',
+        'dest_endpoint',
+        'dest_path',
+    ]
